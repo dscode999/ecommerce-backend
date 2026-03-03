373b562c47277e227d2ee3499d24782e395d8246
dscode999 <dscode999@gmail.com>
2026-03-03 15:47:24 +0200

    Complete: Orders feature with pagination (Admin)

--- src/server.js
+++ src/server.js
@@ -2,25 +2,27 @@ import express from 'express'
 import { connectDB } from './config/db.js';
 import authRout from './features/auth/routes/auth.route.js';
 import productRouter from './features/products/routes/product.route.js';
+import orderRouter from './features/orders/routes/order.route.js';
 
 
 const app = express();
-const port=3010
+const port = 3010
 
 app.use(express.json());
 
 app.use("/api/v1/auth", authRout)
-app.use("/api/v1/product",productRouter)
+app.use("/api/v1/product", productRouter)
+app.use("/api/v1/order", orderRouter)
 app.use("/", (req, res) => {
     res.send("fallback-404 Erorr")
 })
 
 await connectDB()
-  .then(() => {
-      app.listen(port, () => {
-          console.log(`Server is running on port: ${port}`);
-      });
-  })
-  .catch((err) => {
-      console.error("Database connection failed:", err);
-  });
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`Server is running on port: ${port}`);
+        });
+    })
+    .catch((err) => {
+        console.error("Database connection failed:", err);
+    });
