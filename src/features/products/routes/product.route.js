595b7b90c24a98301cfd09f94edd32e9134cd84a
dscode999 <dscode999@gmail.com>
2026-03-10 16:20:54 +0200

    do the validation schema of the products

--- src/features/products/routes/product.route.js
+++ src/features/products/routes/product.route.js
@@ -9,16 +9,17 @@ import {
   updateProduct
 } from '../controller/products.controller.js'
 import { authenticate, authorize } from '../../../shared/middleware/auth.middleware.js'
-
+import { validate } from '../../../shared/middleware/validate.middleware.js'
+import { createProductSchema, ratingSchema, updateProductSchema } from '../product.schema.js'
 const productRouter = express.Router()
 
 productRouter.get('/', getAllProducts)
 productRouter.get('/category/:cat', getProductsByCatgory)
 productRouter.get('/:id', getProductById)
 
-productRouter.post('/', authenticate, authorize('admin'), createProduct)
-productRouter.put('/:id', authenticate, authorize('admin'), updateProduct)
+productRouter.post('/', authenticate, authorize('admin'), validate(createProductSchema), createProduct)
+productRouter.put('/:id', authenticate, authorize('admin'), validate(updateProductSchema), updateProduct)
 productRouter.delete('/:id', authenticate, authorize('admin'), deleteProduct)
-productRouter.post('/:id/rating', authenticate, authorize('customer'), addProductReview)
+productRouter.post('/:id/rating', authenticate, authorize('customer'), validate(ratingSchema), addProductReview)
 
 export default productRouter
