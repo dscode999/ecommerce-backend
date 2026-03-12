ea85fcefb73fd131cb73b3392eed9f3a537c5132
dscode999 <dscode999@gmail.com>
2026-03-12 16:08:03 +0200

    feat: add all Joi schemas and middleware (validate, errorHandler, rateLimiter)

--- src/features/cart/routes/cart.route.js
+++ src/features/cart/routes/cart.route.js
@@ -1,17 +1,19 @@
 import express from 'express'
 import { authenticate, authorize } from '../../../shared/middleware/auth.middleware.js'
 import { addToCart, clearCart, deleteItemFromCart, getCart, syncCart, updateCartItemQuantity } from '../controllers/cart.controller.js'
+import {validate}from '../../../shared/middleware/validate.middleware.js'
+import { addToCartSchema, syncCartSchema } from '../cart.schemas.js'
 
 
 
 const cartRoute=express.Router()
 
 cartRoute.get('/',authenticate,authorize('customer'),getCart)
-cartRoute.post('/',authenticate,authorize('customer'),addToCart)
+cartRoute.post('/',authenticate,authorize('customer'),validate(addToCartSchema),addToCart)
 cartRoute.put('/:productId',authenticate,authorize('customer'),updateCartItemQuantity)
 cartRoute.delete('/:productId',authenticate,authorize('customer'),deleteItemFromCart)
 cartRoute.delete('/',authenticate,authorize('customer'),clearCart)
-cartRoute.post('/sync',authenticate,authorize('customer'),syncCart)
+cartRoute.post('/sync',authenticate,authorize('customer'),validate(syncCartSchema),syncCart)
 
 
 
