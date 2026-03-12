b738fd7e632ed6f670165b5ff11f94318df200b9
dscode999 <dscode999@gmail.com>
2026-03-12 03:38:58 +0200

    feat: add user schemas and connect Joi validation to user routes

--- src/features/users/routes/user.route.js
+++ src/features/users/routes/user.route.js
@@ -1,12 +1,14 @@
 import express from 'express'
 import { authenticate, authorize } from '../../../shared/middleware/auth.middleware.js'
 import { addAddress, changePassword, deleteAddress, deleteUser, getAllUsers, getProfile, updateAddress, updateProfile, updateUserRole } from '../controller/user.controller.js'
+import {validate} from '../../../shared/middleware/validate.middleware.js'
+import { addAddressSchema, changePasswordSchema, updateProfileSchema } from '../user.schema.js'
 const userRoute = express.Router()
 //=====User-PART
 userRoute.get('/profile', authenticate, authorize('customer'), getProfile)
-userRoute.put('/profile', authenticate, authorize('customer'), updateProfile)
-userRoute.put('/change-password', authenticate, authorize('customer'), changePassword)
-userRoute.post('/addresses', authenticate, authorize('customer'), addAddress)
+userRoute.put('/profile', authenticate, authorize('customer'),validate(updateProfileSchema) ,updateProfile)
+userRoute.put('/change-password', authenticate, authorize('customer'),validate(changePasswordSchema) ,changePassword)
+userRoute.post('/addresses', authenticate, authorize('customer'),validate(addAddressSchema) ,addAddress)
 userRoute.put('/addresses/:addrId', authenticate, authorize('customer',), updateAddress)
 userRoute.delete('/addresses/:addrId', authenticate, authorize('customer'), deleteAddress)
 //====Admin-PART
